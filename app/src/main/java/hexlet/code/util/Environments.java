b0e80fc7aa7423189e073053428c7ce25859507b
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2023-11-14 15:32:59 +0300

    Update Environments.java

--- app/src/main/java/hexlet/code/util/Environments.java
+++ app/src/main/java/hexlet/code/util/Environments.java
@@ -17,6 +17,7 @@ public class Environments {
                 parsed = Integer.parseInt(env);
             } catch (NumberFormatException e) {
                 log.error(e.getMessage());
+                log.info("Set application port to default \"8080\"");
             }
             APPLICATION_PORT = parsed;
         }
