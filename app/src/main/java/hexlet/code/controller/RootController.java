6898902ca860f44485a8211c46904dcd55c908d1
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2023-11-15 15:03:15 +0300

    Update layout

--- app/src/main/java/hexlet/code/controller/RootController.java
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -5,12 +5,12 @@ import io.javalin.http.Context;
 
 import java.util.Collections;
 
-import static hexlet.code.util.Timestamp.getTimestamp;
+import static hexlet.code.util.Timestamp.getPageTimestamp;
 
 public final class RootController {
     public static void index(Context ctx) {
         var page = new RootPage();
-        page.setDate(getTimestamp());
+        page.setDate(getPageTimestamp());
         ctx.render("index.jte", Collections.singletonMap("page", page));
     }
 }
