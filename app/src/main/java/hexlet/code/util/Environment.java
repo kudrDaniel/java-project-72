cca6a668b806e1c974ca6fc71581ca4fabbcb1f9
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2023-11-16 19:24:38 +0300

    Fix checkstyle issue

--- app/src/main/java/hexlet/code/util/Environment.java
+++ app/src/main/java/hexlet/code/util/Environment.java
@@ -5,13 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Environment {
     public static int getPort() {
-        String raw = System.getenv().getOrDefault("PORT", "7080");
-        int result = 7080;
+        String raw = System.getenv().getOrDefault("PORT", "7090");
+        int result = 7090;
         try {
             result = Integer.parseInt(raw);
         } catch (NumberFormatException e) {
             log.error(e.getMessage());
-            log.info("Port set to default: 7080");
+            log.info("Port set to default: 7090");
         }
         return result;
     }
@@ -19,7 +19,7 @@ public class Environment {
     public static String getJdbcUrl() {
         return System.getenv().getOrDefault("JDBC_DATABASE_URL", getMemJdbc());
     }
-    
+
     public static String getMemJdbc() {
         return "jdbc:h2:mem:seo_page;DB_CLOSE_DELAY=-1;";
     }
